afda4e2b68c76c2d45ec3011799ff8b122663f13
Klarnner <annaklepikovae@gmail.com>
2024-01-05 18:29:15 +0300

    added conflict file

--- conflictFile.ts
+++ conflictFile.ts
@@ -1,12 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-function expect(linkElement: HTMLElement) {
-    throw new Error('Function not implemented.');
-}
-
+const reportWebVitals = onPerfEntry => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+      import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+        getCLS(onPerfEntry);
+      });
+    }
+  };
+  
+  export default reportWebVitals;
